168567718c7a6780a0eba1222bfc25cf68f8459f
dinesh-2047 <dinesh07bhardwaj@gmail.com>
2025-10-11 00:09:18 +0530

    11th october maximum path sum

---
+++ Binary Tree/11th October - Maximum path sum.cpp
@@ -0,0 +1,42 @@
+// Maximum path sum
+
+/*
+class Node {
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+  int maxSum ;
+  int solve(Node* root){
+      if(root == nullptr ) return 0;
+      
+      int l = solve(root->left);
+      int r = solve(root->right);
+      
+      int neeche_hi_milgya_answer = l +r + root->data;
+      
+      int koi_ek_acha = max(l,r) + root->data;
+      int only_root_acha = root->data;
+      
+      maxSum = max({maxSum , neeche_hi_milgya_answer, koi_ek_acha, only_root_acha});
+      
+      //most important part ; 
+      return max(koi_ek_acha, only_root_acha);
+  }
+  
+  
+    int findMaxSum(Node *root) {
+        maxSum = INT_MIN;
+        solve(root);
+        return maxSum;
+    }
+};
