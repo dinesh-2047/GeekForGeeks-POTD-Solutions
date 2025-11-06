496e429844d9eed9c1417e3bf8b8eecab24f4ea7
dinesh-2047 <dinesh07bhardwaj@gmail.com>
2025-11-07 01:15:30 +0530

    Weighted Job Scheduling 7th nov

---
+++ Dynamic Programming/7th Nov - Weighted Job Scheduling.cpp
@@ -0,0 +1,39 @@
+// Weighted Job Scheduling
+
+
+class Solution {
+  public:
+    int BS(int idx,int target,vector<vector<int>> &jobs){
+        int n=jobs.size();
+        int result=n;
+        int s=idx,e=n-1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(jobs[mid][0]>=target){
+              result= mid;
+               e=mid-1;
+            }
+            else {
+                s=mid+1;
+            }
+        }
+        return result;
+    }
+   
+    int solve(int i,vector<vector<int>> &jobs,vector<int>&dp){
+        if(i>=jobs.size()) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int inc=0;
+        int nextIdx=BS(i+1,jobs[i][1],jobs);
+        inc=jobs[i][2]+solve(nextIdx,jobs,dp);
+        int exc=solve(i+1,jobs,dp);
+        return dp[i]=max(inc,exc);
+    }
+    int maxProfit(vector<vector<int>> &jobs) {
+        sort(jobs.begin(),jobs.end());
+        vector<int>dp(jobs.size()+1,-1);
+        return solve(0,jobs,dp);
+    }
+};
+        
+
