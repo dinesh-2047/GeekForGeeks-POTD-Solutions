6ec1c04bef336b63454426f3c24828ea90915f43
dinesh-2047 <dinesh07bhardwaj@gmail.com>
2025-12-12 02:05:30 +0530

    Transpose of Matrix 12th dec 2025

---
+++ Easy/12th Dec - Transpose of Matrix.cpp
@@ -0,0 +1,14 @@
+// Transpose of Matrix
+
+class Solution {
+  public:
+    vector<vector<int>> transpose(vector<vector<int>>& mat) {
+        int n = mat.size();
+         for(int i = 0 ; i < n; i++){
+             for(int j = i ;j < n; j++){
+                 swap(mat[i][j] , mat[j][i]);
+             }
+         } 
+        return mat; 
+    }
+};
