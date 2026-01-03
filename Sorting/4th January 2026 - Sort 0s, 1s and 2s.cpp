d510c5fb13664a5a1f67b7284f7057cda13ccc7b
dinesh-2047 <dinesh07bhardwaj@gmail.com>
2026-01-04 00:46:40 +0530

    4th January 2026 - Sort 0s, 1s and 2s.cpp

---
+++ Sorting/4th January 2026 - Sort 0s, 1s and 2s.cpp
@@ -0,0 +1,32 @@
+// 4th January 2026 - Sort 0s, 1s and 2s.cpp
+
+
+class Solution {
+  public:
+    void sort012(vector<int>& arr) {
+      int n = arr.size();
+      
+      int l = 0 ;
+      int m = 0 ; 
+      int r = n-1; 
+      
+      
+      while(m<=r){
+          if(arr[m] == 2 ){
+              swap(arr[m], arr[r]);
+              r--;
+              
+          }
+          else if(arr[m] == 0){
+              swap(arr[m], arr[l]);
+              l++;
+              m++;
+          }
+          else{
+              m++;
+          }
+      }
+        
+        
+    }
+};
