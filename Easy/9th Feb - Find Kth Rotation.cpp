562ba6408629d1814b926b46637bf00850e17643
dinesh-2047 <dinesh07bhardwaj@gmail.com>
2026-02-09 01:57:51 +0530

    Find Kth Rotation 9th feb

---
+++ Easy/9th Feb - Find Kth Rotation.cpp
@@ -0,0 +1,22 @@
+// Find Kth Rotation
+
+class Solution {
+  public:
+    int findKRotation(vector<int> &arr) {
+          int n = arr.size();
+          
+          int l = 0 ; 
+          int r = n-1 ; 
+          
+          while( l < r ){
+              int mid = l +  ( r - l )/2; 
+              
+              if(arr[mid] < arr[r]){
+                  r = mid ; 
+              }
+              else l = mid + 1; 
+          }
+          return l; 
+        
+    }
+};
