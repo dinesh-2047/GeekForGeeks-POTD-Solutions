e5969eead3e8cff11c2202c485e36966f338caf6
dinesh-2047 <dinesh07bhardwaj@gmail.com>
2026-03-03 00:34:18 +0530

    Longest subarray with Atmost two distinct integers  3rd march 26

---
+++ Sliding Window/3rd March 26 - Longest subarray with Atmost two distinct integers.cpp
@@ -0,0 +1,30 @@
+// Longest subarray with Atmost two distinct integers
+
+class Solution {
+  public:
+    int totalElements(vector<int> &arr) {
+        
+        int n = arr.size();
+        
+        unordered_map<int , int>mp; 
+        
+        int i  = 0 ; 
+        int j = 0 ; 
+        int result = 0 ;
+        while(j < n ){
+            mp[arr[j]]++;
+            while(i <= j && mp.size() > 2){
+                mp[arr[i]]--;
+                if(mp[arr[i]] == 0 ){
+                    mp.erase(arr[i]);
+                }
+                i++;
+            }
+            result = max(result, j - i + 1);
+            j++;
+            
+        }
+        return result;
+        
+    }
+};
