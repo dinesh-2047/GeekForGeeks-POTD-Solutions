6d98eb044764706b232e818cf61b0780a0181350
dinesh-2047 <dinesh07bhardwaj@gmail.com>
2026-03-05 00:32:13 +0530

    Longest Substring with K Uniques 26 march

---
+++ Sliding Window/5th March 26 - Longest Substring with K Uniques.cpp
@@ -0,0 +1,28 @@
+// Longest Substring with K Uniques
+
+class Solution {
+  public:
+    int longestKSubstr(string &s, int k) {
+        
+        int n = s.length();
+        
+        unordered_map<char, int>mp; 
+        
+        int i = 0 ; 
+        int j =0  ; 
+        
+        int result = -1 ; 
+        
+        while( j < n ){
+            mp[s[j]]++;
+            if(mp.size() == k) result = max(result , j - i + 1);
+            while( i <= j && mp.size() > k ){
+                mp[s[i]]--;
+                if(mp[s[i]] == 0 ) mp.erase(s[i]);
+                i++;
+            }
+            j++;
+        }
+        return result;
+    }
+};
