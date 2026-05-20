2fa882ca44a5098bcad4f19b6c29ba9b1d441f22
dinesh-2047 <dinesh07bhardwaj@gmail.com>
2026-05-21 00:28:15 +0530

    Check if All Bits Set  21st may 26

---
+++ Basic/21st May 26  - Check if All Bits Set.cpp
@@ -0,0 +1,12 @@
+// Check if All Bits Set
+
+class Solution {
+  public:
+    bool isBitSet(int n) {
+        
+        if(n == 0 ) return 0 ; 
+        return (n&(n+1)) == 0 ;
+        
+        
+    }
+};
