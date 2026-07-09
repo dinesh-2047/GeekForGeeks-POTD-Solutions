ebe275a001c677f6b83115be201817e048203181
dinesh-2047 <dinesh07bhardwaj@gmail.com>
2026-07-10 01:48:16 +0530

    Ways to Express as Sum of Consecutives  10th july 26

---
+++ Maths/10th July 26 - Ways to Express as Sum of Consecutives.cpp
@@ -0,0 +1,19 @@
+// Ways to Express as Sum of Consecutives
+
+class Solution {
+  public:
+    int getCount(int n) {
+        int i = 1; 
+        int j = 0 ; 
+        
+        int k = 0 ; 
+        
+        while(n > 0 ){
+            n = n - i ; 
+            if(n%i == 0 ) k++;
+            i++;
+        }
+        return k - 1 ; 
+        
+    }
+};
